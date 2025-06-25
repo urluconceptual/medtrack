288175866313c9b1ebb7df2a527009de7d9c9873
Elena Georgescu <101599503+elenaag23@users.noreply.github.com>
2025-06-26 01:22:15 +0300

    updated database file

--- app/src/main/java/com/unibuc/medtrack/data/AppDatabase.kt
+++ app/src/main/java/com/unibuc/medtrack/data/AppDatabase.kt
@@ -2,23 +2,29 @@ package com.unibuc.medtrack.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.unibuc.medtrack.data.dao.UsersDAO
 import com.unibuc.medtrack.data.dao.DoctorsDAO
 import com.unibuc.medtrack.data.dao.PatientsDAO
+import com.unibuc.medtrack.data.dao.TreatmentsDAO
 import com.unibuc.medtrack.data.models.UserModel
 import com.unibuc.medtrack.data.models.DoctorModel
 import com.unibuc.medtrack.data.models.PatientModel
+import com.unibuc.medtrack.data.models.TreatmentModel
 
 @Database(
     entities = [
         UserModel::class,
         DoctorModel::class,
-        PatientModel::class
+        PatientModel::class,
+        TreatmentModel::class,
     ],
     version = 4
 )
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract val usersDao: UsersDAO
     abstract val doctorsDao: DoctorsDAO
     abstract val patientsDao: PatientsDAO
+    abstract val treatmentsDAO: TreatmentsDAO
 }
