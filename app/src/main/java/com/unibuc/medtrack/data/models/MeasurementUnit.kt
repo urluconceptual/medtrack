dbd5fd00e86dc0a88ef11a403f9406ca246ec328
Elena Georgescu <101599503+elenaag23@users.noreply.github.com>
2025-06-26 01:21:27 +0300

    Added treatments

---
+++ app/src/main/java/com/unibuc/medtrack/data/models/MeasurementUnit.kt
@@ -0,0 +1,5 @@
+package com.unibuc.medtrack.data.models
+
+enum class MeasurementUnit {
+    ML, MG, G, TBSP, TABLET, CAPSULE, DROP, PUFF
+}
--- app/src/main/java/com/unibuc/medtrack/ui/home/PatientHomeFragment.kt
+++ app/src/main/java/com/unibuc/medtrack/ui/home/PatientHomeFragment.kt
@@ -2,15 +2,20 @@ package com.unibuc.medtrack.ui.home
 
 import android.content.res.ColorStateList
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.unibuc.medtrack.R
+import com.unibuc.medtrack.data.models.TreatmentModel
 import dagger.hilt.android.AndroidEntryPoint
 import java.text.SimpleDateFormat
 import java.time.LocalDate
@@ -26,9 +31,12 @@ class PatientHomeFragment : Fragment() {
     ): View? = inflater.inflate(R.layout.fragment_patient_home, container, false)
 
     private val userViewModel: PatientHomeViewModel by viewModels()
+    private val viewModel: PatientCalendarViewModel by viewModels()
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        fetchTreatments()
         setupData()
     }
 
@@ -96,4 +104,31 @@ class PatientHomeFragment : Fragment() {
             }
         }
     }
+
+    private fun fetchTreatments() {
+        Log.i("PatientHomeFragment", "registering observers")
+
+        viewModel.treatments.observe(viewLifecycleOwner) { treatmentList ->
+            Log.i("PatientHomeFragment", "full treatment list: $treatmentList")
+            bindTreatmentsToUI(treatmentList)
+        }
+
+        viewModel.error.observe(viewLifecycleOwner) { errorMessage ->
+            errorMessage?.let {
+                Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        Log.i("PatientHomeFragment", "calling fetchFullTreatmentsForPatient()")
+        viewModel.fetchTreatmentsForPatient()  // This should now return List<FullTreatment>
+    }
+
+
+
+    private fun bindTreatmentsToUI(treatments: List<TreatmentModel>) {
+        val recyclerView = view?.findViewById<RecyclerView>(R.id.treatments_recycler)
+        recyclerView?.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView?.adapter = TreatmentAdapter(treatments)
+    }
+
 }
