31475b4c8ed055eedfb48cec333d8c544a6c634b
Elena Georgescu <101599503+elenaag23@users.noreply.github.com>
2025-06-26 20:34:04 +0300

    Added db modifications

--- app/src/main/java/com/unibuc/medtrack/data/AppDatabase.kt
+++ app/src/main/java/com/unibuc/medtrack/data/AppDatabase.kt
@@ -6,11 +6,13 @@ import com.unibuc.medtrack.data.dao.ChatMessagesDAO
 import androidx.room.TypeConverters
 import com.unibuc.medtrack.data.dao.UsersDAO
 import com.unibuc.medtrack.data.dao.DoctorsDAO
+import com.unibuc.medtrack.data.dao.NotificationsDAO
 import com.unibuc.medtrack.data.dao.PatientsDAO
 import com.unibuc.medtrack.data.models.ChatMessageModel
 import com.unibuc.medtrack.data.dao.TreatmentsDAO
 import com.unibuc.medtrack.data.models.UserModel
 import com.unibuc.medtrack.data.models.DoctorModel
+import com.unibuc.medtrack.data.models.NotificationModel
 import com.unibuc.medtrack.data.models.PatientModel
 import com.unibuc.medtrack.data.models.TreatmentModel
 
@@ -20,7 +22,8 @@ import com.unibuc.medtrack.data.models.TreatmentModel
         DoctorModel::class,
         PatientModel::class,
         ChatMessageModel::class,
-        TreatmentModel::class
+        TreatmentModel::class,
+        NotificationModel::class
     ],
     version = 5
 )
@@ -31,4 +34,5 @@ abstract class AppDatabase : RoomDatabase() {
     abstract val patientsDao: PatientsDAO
     abstract val chatMessageDao: ChatMessagesDAO
     abstract val treatmentsDAO: TreatmentsDAO
+    abstract val notificationsDAO: NotificationsDAO
 }
