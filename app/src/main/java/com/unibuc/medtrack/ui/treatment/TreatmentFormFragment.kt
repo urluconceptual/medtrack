69365a2b18f47623a910a80f62600ded870b8a84
Alexandra-Diana Ciocan <ciocan_alexandra16@yahoo.com>
2025-06-27 09:47:11 +0300

    refine UI

--- app/src/main/java/com/unibuc/medtrack/ui/treatment/TreatmentFormFragment.kt
+++ app/src/main/java/com/unibuc/medtrack/ui/treatment/TreatmentFormFragment.kt
@@ -1,4 +1,24 @@
 package com.unibuc.medtrack.ui.treatment
 
-class TreatmentFormFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.unibuc.medtrack.R
+
+class TreatmentFormFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_treatment_form, container, false)
+
+    private val viewModel: TreatmentFormViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
